869d5883dc4fbc5156bc6f327e68abf7d2bd2924
Jan Bladt <jan.bladt@syspons.com>
2023-06-26 15:32:43 +0200

    use targetable

--- src/Services/TranslatableService.php
+++ src/Services/TranslatableService.php
@@ -2,71 +2,19 @@
 
 namespace berthott\Translatable\Services;
 
-use HaydenPierce\ClassFinder\ClassFinder;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Str;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Cache;
+use berthott\Targetable\Services\TargetableService;
+use berthott\Translatable\Facades\Translatable;
 
-const CACHE_KEY = 'TranslatableService-Cache-Key';
-
-class TranslatableService
+/**
+ * TargetableService implementation for an targetable class.
+ * 
+ * @link https://docs.syspons-dev.com/laravel-targetable
+ */
+class TranslatableService extends TargetableService
 {
-    /**
-     * Collection with all translatable classes.
-     */
-    private Collection $translatables;
-
-    /**
-     * The Constructor.
-     */
     public function __construct()
     {
-        $this->initTranslatableClasses();
-    }
-
-    /**
-     * Get the translatable classes collection.
-     */
-    public function getTranslatableClasses(): Collection
-    {
-        return $this->translatables;
-    }
-
-    /**
-     * Initialize the translatable classes collection.
-     */
-    private function initTranslatableClasses(): void
-    {
-        $this->translatables = Cache::sear(CACHE_KEY, function () {
-            $translatables = [];
-            $namespaces = config('translatable.namespace');
-            foreach (is_array($namespaces) ? $namespaces : [$namespaces] as $namespace) {
-                foreach (ClassFinder::getClassesInNamespace($namespace, config('translatable.namespace_mode')) as $class) {
-                    foreach (class_uses_recursive($class) as $trait) {
-                        if ('berthott\Translatable\Models\Traits\Translatable' == $trait) {
-                            array_push($translatables, $class);
-                        }
-                    }
-                }
-            }
-            return collect($translatables);
-        });
-    }
-
-    /**
-     * Get the target model.
-     */
-    public function getTarget(): string
-    {
-        if (!request()->segments() || $this->translatables->isEmpty()) {
-            return '';
-        }
-        $model = Str::studly(Str::singular(request()->segment(count(explode('/', config('permissions.prefix'))) + 1)));
-
-        return $this->translatables->first(function ($class) use ($model) {
-            return Arr::last(explode('\\', $class)) === $model;
-        }) ?: '';
+        parent::__construct(Translatable::class, 'translatable');
     }
 
     /**
